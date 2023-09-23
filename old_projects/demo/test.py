c4fdab304ccf4617fc602402fb0853f05276c3df
Thomas Kragerud <thomas.kragerud@gmail.com>
2023-09-22 22:24:27 -0700

    fungerer med bilder

---
+++ old_projects/demo/test.py
@@ -0,0 +1,6 @@
+my_str = "0fordi"
+
+if my_str[0] == "0":
+    my_str = my_str[1:]
+
+print(my_str)
--- src/backend/app/views/main.py
+++ src/backend/app/views/main.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, jsonify, render_template, request
+from flask import Blueprint, jsonify, render_template, request, url_for
 import random
 import geopandas as gpd
 import json
 from random import choice
+import os
+import glob
 
 
 # routs are now defined in the context of this Blueprint
@@ -24,11 +26,29 @@ def get_geojson():
     return jsonify(data)
 
 
+
 @bp.route('/get-random-municipality')
 def get_random_municipality():
-    with open('src/backend/app/data/Kommuner-large.json') as f:
+    with open('src/frontend/static/data/Kommuner-large.json') as f:
         data = json.load(f)
+
     random_municipality = choice(data['features'])
     name = random_municipality['properties']['navn']
-    icon_path = f'kjoh'
+    number = str(random_municipality['properties']['kommunenummer'])
+
+    if number[0] == "0":
+        number = number[1:]
+
+    # Search for the file based on the starting number
+    search_path = os.path.join('src', 'frontend', 'static', 'data', 'Kommuner', 'All', f"{number}*.svg")
+    matching_files = glob.glob(search_path)
+
+    if not matching_files:
+        # If no matching file is found, return an error or a default image
+        return jsonify(error="Image not found"), 404
+
+    # Take the first matching file
+    icon_filename = os.path.basename(matching_files[0])
+    icon_path = url_for('static', filename=f'data/Kommuner/All/{icon_filename}')
+
     return jsonify(name=name, icon_url=icon_path)
